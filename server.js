47b97b8bbbbb7adc3b89583ce2226eb76ba4bb10
Sandhya Bezawada <sandhya.bezawada@qentelli.com>
2022-01-25 17:02:49 +0530

    firstapicreated

---
+++ server.js
@@ -0,0 +1,13 @@
+// importing expressjs
+
+const  express = require('express')
+const app = express()
+
+app.get('/',(req,res)=>{
+        res.send('Hello World')
+})
+ const PORT=3000
+
+ app.listen(PORT,()=>{
+     console.log(`server running at port ${PORT}`)
+ })
